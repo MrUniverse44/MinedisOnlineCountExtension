1db94d1630038e11245977ef5581bb5b1bdaa63c
MrUniverse44 <oficial.mruniverse@gmail.com>
2023-12-31 11:51:46 -0500

    feat: structure

--- src/main/java/me/blueslime/minedisonline/MinedisOnline.java
+++ src/main/java/me/blueslime/minedisonline/MinedisOnline.java
@@ -1,16 +1,178 @@
 package me.blueslime.minedisonline;
 
-import net.md_5.bungee.api.plugin.Plugin;
+import me.blueslime.minedis.api.extension.MinedisExtension;
+import me.blueslime.minedisonline.listener.DiscordCommandListener;
+import net.dv8tion.jda.api.entities.Guild;
+import net.dv8tion.jda.api.interactions.commands.build.Commands;
 
-public final class MinedisOnline extends Plugin {
+import java.util.ArrayList;
+
+public final class MinedisOnline extends MinedisExtension {
+
+    private final ArrayList<String> staffList = new ArrayList<>();
+    private final ArrayList<String> onlineList = new ArrayList<>();
 
     @Override
-    public void onEnable() {
-        // Plugin startup logic
+    public String getIdentifier() {
+        return "MOnlineList";
+    }
+
+    @Override
+    public String getName() {
+        return "Minedis Online";
+    }
+
+    @Override
+    public void onEnabled() {
+        String embedPath = "settings.commands.staff-online.";
+        if (!getConfiguration().contains("settings.staff-permission")) {
+            getConfiguration().set("settings.staff-permission", "minedis.online.staff");
+        }
+        if (!getConfiguration().contains(embedPath + "command")) {
+            getConfiguration().set(embedPath + "command", "os");
+            getConfiguration().set(embedPath + "command-description", "Check your staff list");
+            getConfiguration().set(embedPath + "guild-id", "NOT_SET");
+            getConfiguration().set(embedPath + "title", "Server Online Staff");
+            getConfiguration().set(embedPath + "description", "Currently **%staff% staff(s) online and %users% user(s) online.");
+            getConfiguration().set(embedPath + "color", "YELLOW");
+            getConfiguration().set(embedPath + "footer", "mc.spigotmc.org");
+        }
+
+        embedPath = "settings.commands.default-online.";
+        if (!getConfiguration().contains(embedPath + "command")) {
+            getConfiguration().set(embedPath + "command", "online");
+            getConfiguration().set(embedPath + "command-description", "Check your online list");
+            getConfiguration().set(embedPath + "guild-id", "NOT_SET");
+            getConfiguration().set(embedPath + "title", "Server Online");
+            getConfiguration().set(embedPath + "description", "Currently %users% user(s) online.");
+            getConfiguration().set(embedPath + "color", "BLUE");
+            getConfiguration().set(embedPath + "footer", "mc.spigotmc.org");
+        }
+
+        saveConfiguration();
+
+        registerEventListeners(
+            new DiscordCommandListener(this)
+        );
+
+        String guildID = getConfiguration().getString(embedPath + "guild-id", "NOT_SET");
+
+        if (guildID.isEmpty() || guildID.equalsIgnoreCase("NOT_SET")) {
+            getLogger().info("Can't register link command because discord guild id was not set yet.");
+            return;
+        }
+
+        Guild guild = getJDA().getGuildById(guildID);
+
+        if (guild == null) {
+            getLogger().info("Discord GUILD was not found for link command.");
+            return;
+        }
+
+        String description = getConfiguration().getString(embedPath + "command-description", "Check your online list");
+
+        guild.upsertCommand(
+                Commands.slash(
+                    getConfiguration().getString(embedPath + "command", "online"),
+                    description
+                )
+        ).queue(cmd -> onlineList.add(cmd.getId()));
+
+        if (getConfiguration().contains(embedPath + "command-aliases")) {
+            for (String command : getConfiguration().getStringList(embedPath + "command-aliases")) {
+                guild.upsertCommand(
+                        Commands.slash(
+                            command,
+                            description
+                        )
+                ).queue(cmd -> onlineList.add(cmd.getId()));
+            }
+        }
+
+        embedPath = "settings.commands.staff-online.";
+
+        description = getConfiguration().getString(embedPath + "command-description", "Check your staff list");
+
+        guildID = getConfiguration().getString(embedPath + "guild-id", "NOT_SET");
+
+        if (guildID.isEmpty() || guildID.equalsIgnoreCase("NOT_SET")) {
+            getLogger().info("Can't register link command because discord guild id was not set yet.");
+            return;
+        }
+
+        guild = getJDA().getGuildById(guildID);
+
+        if (guild == null) {
+            getLogger().info("Discord GUILD was not found for link command.");
+            return;
+        }
+
+        guild.upsertCommand(
+                Commands.slash(
+                        getConfiguration().getString(embedPath + "command", "os"),
+                        description
+                )
+        ).queue(cmd -> staffList.add(cmd.getId()));
+
+        if (getConfiguration().contains(embedPath + "command-aliases")) {
+            for (String command : getConfiguration().getStringList(embedPath + "command-aliases")) {
+                guild.upsertCommand(
+                        Commands.slash(
+                                command,
+                                description
+                        )
+                ).queue(cmd -> staffList.add(cmd.getId()));
+            }
+        }
     }
 
     @Override
     public void onDisable() {
-        // Plugin shutdown logic
+        getLogger().info("All listeners are unloaded from Minedis Online");
+
+        String guildID = getConfiguration().getString("settings.commands.staff-online.guild-id", "NOT_SET");
+
+        if (guildID.isEmpty() || guildID.equalsIgnoreCase("NOT_SET")) {
+            getLogger().info("Can't register link command because discord guild id was not set yet.");
+            return;
+        }
+
+        Guild guild = getJDA().getGuildById(guildID);
+
+        if (guild == null) {
+            getLogger().info("Discord GUILD was not found for online command.");
+            return;
+        }
+
+        Guild finalGuild1 = guild;
+        onlineList.forEach(command -> finalGuild1.deleteCommandById(command).queue());
+
+        guildID = getConfiguration().getString("settings.commands.default-online.guild-id", "NOT_SET");
+
+        if (guildID.isEmpty() || guildID.equalsIgnoreCase("NOT_SET")) {
+            getLogger().info("Can't register link command because discord guild id was not set yet.");
+            return;
+        }
+
+        guild = getJDA().getGuildById(guildID);
+
+        if (guild == null) {
+            getLogger().info("Discord GUILD was not found for staff online command.");
+            return;
+        }
+
+        Guild finalGuild = guild;
+        staffList.forEach(command -> finalGuild.deleteCommandById(command).queue());
+
+        onlineList.clear();
+        staffList.clear();
+    }
+
+    public ArrayList<String> getOnlineList() {
+        return onlineList;
+    }
+
+    public ArrayList<String> getStaffList() {
+        return staffList;
     }
 }
--- src/main/java/me/blueslime/minedisonline/listener/DiscordCommandListener.java
+++ src/main/java/me/blueslime/minedisonline/listener/DiscordCommandListener.java
@@ -1,2 +1,56 @@
-package me.blueslime.minedisonline.listener;public class DiscordCommandListener {
+package me.blueslime.minedisonline.listener;
+
+import me.blueslime.minedis.utils.text.TextReplacer;
+import me.blueslime.minedisonline.MinedisOnline;
+import me.blueslime.minedisonline.utils.EmbedSection;
+import net.dv8tion.jda.api.events.interaction.command.SlashCommandInteractionEvent;
+import net.dv8tion.jda.api.hooks.ListenerAdapter;
+import net.md_5.bungee.api.connection.ProxiedPlayer;
+import org.jetbrains.annotations.NotNull;
+
+import java.util.concurrent.TimeUnit;
+
+public class DiscordCommandListener extends ListenerAdapter {
+    private final MinedisOnline plugin;
+
+    public DiscordCommandListener(MinedisOnline plugin) {
+        this.plugin = plugin;
+    }
+
+    @Override
+    public void onSlashCommandInteraction(@NotNull SlashCommandInteractionEvent event) {
+        if (plugin.getOnlineList().contains(event.getName()) || plugin.getStaffList().contains(event.getName())) {
+
+            String permission = plugin.getConfiguration().getString("settings.staff-permission", "minedis.online.staff");
+
+            int staff = 0;
+            int users = 0;
+
+            for (ProxiedPlayer player : plugin.getProxy().getPlayers()) {
+                if (player.hasPermission(permission)) {
+                    staff++;
+                    continue;
+                }
+                users++;
+            }
+
+            String path = plugin.getOnlineList().contains(event.getName()) ?
+                    "settings.commands.default-online" : "settings.commands.staff-online";
+
+            event.deferReply(true).queue();
+
+            TextReplacer replacer = TextReplacer.builder()
+                .replace("%staff%", String.valueOf(staff))
+                .replace("%users%", String.valueOf(users))
+                .replace("%user%", String.valueOf(users))
+                .replace("%staffs%", String.valueOf(staff))
+                .replace("%players%", String.valueOf(users));
+
+            event.getHook().setEphemeral(true).sendMessageEmbeds(
+                new EmbedSection(
+                    plugin.getConfiguration().getSection(path)
+                ).build(replacer)
+            ).queueAfter(1, TimeUnit.SECONDS);
+        }
+    }
 }
