46929f1c12c5a5db75bff44bb45158ae4e9d25c3
MrUniverse44 <oficial.mruniverse@gmail.com>
2024-04-07 23:32:07 -0500

    feat: support  for minedis 1.0.4

--- src/main/java/me/blueslime/minedisonline/MinedisOnline.java
+++ src/main/java/me/blueslime/minedisonline/MinedisOnline.java
@@ -1,6 +1,7 @@
 package me.blueslime.minedisonline;
 
 import me.blueslime.minedis.api.extension.MinedisExtension;
+import me.blueslime.minedis.utils.consumer.PluginConsumer;
 import me.blueslime.minedisonline.listener.DiscordCommandListener;
 import net.dv8tion.jda.api.entities.Guild;
 import net.dv8tion.jda.api.interactions.commands.build.Commands;
@@ -64,98 +65,108 @@ public final class MinedisOnline extends MinedisExtension {
     }
 
     public void registerOnlineCommand(String embedPath) {
-        String guildID = getConfiguration().getString(embedPath + "guild-id", "NOT_SET");
+        PluginConsumer.process(
+            () -> {
+                String guildID = getConfiguration().getString(embedPath + "guild-id", "NOT_SET");
 
-        if (guildID.isEmpty() || guildID.equalsIgnoreCase("NOT_SET")) {
-            getLogger().info("Can't register online command because discord guild id was not set yet.");
-            getLogger().info("Path: " + embedPath + "guild-id, value: " + guildID);
-            return;
-        }
+                if (guildID.isEmpty() || guildID.equalsIgnoreCase("NOT_SET")) {
+                    getLogger().info("Can't register online command because discord guild id was not set yet.");
+                    getLogger().info("Path: " + embedPath + "guild-id, value: " + guildID);
+                    return;
+                }
 
-        Guild guild = getJDA().getGuildById(guildID);
+                Guild guild = getJDA().getGuildById(guildID);
 
-        if (guild == null) {
-            getLogger().info("Discord GUILD was not found for online command.");
-            return;
-        }
+                if (guild == null) {
+                    getLogger().info("Discord GUILD was not found for online command.");
+                    return;
+                }
 
-        getLogger().info("Discord guild-id for online command is: " + guild.getId());
+                getLogger().info("Discord guild-id for online command is: " + guild.getId());
 
-        String description = getConfiguration().getString(embedPath + "command-description", "Check your online list");
+                String description = getConfiguration().getString(embedPath + "command-description", "Check your online list");
 
-        String command = getConfiguration().getString(embedPath + "command", "online");
+                String command = getConfiguration().getString(embedPath + "command", "online");
 
-        onlineCommands.add(command);
+                onlineCommands.add(command);
 
-        registerCommand(
-            guild,
-            Commands.slash(
-                command,
-                description
-            )
-        );
-
-        getLogger().info("Registered command: /" + command + " at guild-id: " + guild.getId());
-
-        if (getConfiguration().contains(embedPath + "command-aliases")) {
-            for (String cmd : getConfiguration().getStringList(embedPath + "command-aliases")) {
-                getLogger().info("Registered command: /" + cmd + " at guild-id: " + guild.getId());
-                onlineCommands.add(cmd);
                 registerCommand(
-                    guild,
-                    Commands.slash(
-                            cmd,
-                            description
-                    )
+                        guild,
+                        Commands.slash(
+                                command,
+                                description
+                        )
                 );
-            }
-        }
+
+                getLogger().info("Registered command: /" + command + " at guild-id: " + guild.getId());
+
+                if (getConfiguration().contains(embedPath + "command-aliases")) {
+                    for (String cmd : getConfiguration().getStringList(embedPath + "command-aliases")) {
+                        getLogger().info("Registered command: /" + cmd + " at guild-id: " + guild.getId());
+                        onlineCommands.add(cmd);
+                        registerCommand(
+                                guild,
+                                Commands.slash(
+                                        cmd,
+                                        description
+                                )
+                        );
+                    }
+                }
+            },
+            e -> getLogger().info("Can't register online command due to issues with bot connection!")
+        );
     }
 
     public void registerStaffOnlineCommand(String embedPath) {
-        String description = getConfiguration().getString(embedPath + "command-description", "Check your staff list");
+        PluginConsumer.process(
+            () -> {
+                String description = getConfiguration().getString(embedPath + "command-description", "Check your staff list");
 
-        String guildID = getConfiguration().getString(embedPath + "guild-id", "NOT_SET");
+                String guildID = getConfiguration().getString(embedPath + "guild-id", "NOT_SET");
 
-        if (guildID.isEmpty() || guildID.equalsIgnoreCase("NOT_SET")) {
-            getLogger().info("Can't register staff online command because discord guild id was not set yet.");
-            getLogger().info("Path: " + embedPath + "guild-id, value: " + guildID);
-            return;
-        }
+                if (guildID.isEmpty() || guildID.equalsIgnoreCase("NOT_SET")) {
+                    getLogger().info("Can't register staff online command because discord guild id was not set yet.");
+                    getLogger().info("Path: " + embedPath + "guild-id, value: " + guildID);
+                    return;
+                }
 
-        Guild guild = getJDA().getGuildById(guildID);
+                Guild guild = getJDA().getGuildById(guildID);
 
-        if (guild == null) {
-            getLogger().info("Discord GUILD was not found for staff online command.");
-            return;
-        }
+                if (guild == null) {
+                    getLogger().info("Discord GUILD was not found for staff online command.");
+                    return;
+                }
 
-        String cmd = getConfiguration().getString(embedPath + "command", "os");
-        getLogger().info("Registered command: /" + cmd + " at guild-id: " + guild.getId());
+                String cmd = getConfiguration().getString(embedPath + "command", "os");
+                getLogger().info("Registered command: /" + cmd + " at guild-id: " + guild.getId());
 
-        staffCommands.add(cmd);
+                staffCommands.add(cmd);
 
-        registerCommand(
-            guild,
-            Commands.slash(
-                cmd,
-                description
-            )
-        );
-
-        if (getConfiguration().contains(embedPath + "command-aliases")) {
-            for (String command : getConfiguration().getStringList(embedPath + "command-aliases")) {
-                getLogger().info("Registered command: /" + command + " at guild-id: " + guild.getId());
-                staffCommands.add(command);
                 registerCommand(
-                    guild,
-                    Commands.slash(
-                        command,
-                        description
-                    )
+                        guild,
+                        Commands.slash(
+                                cmd,
+                                description
+                        )
                 );
-            }
-        }
+
+                if (getConfiguration().contains(embedPath + "command-aliases")) {
+                    for (String command : getConfiguration().getStringList(embedPath + "command-aliases")) {
+                        getLogger().info("Registered command: /" + command + " at guild-id: " + guild.getId());
+                        staffCommands.add(command);
+                        registerCommand(
+                                guild,
+                                Commands.slash(
+                                        command,
+                                        description
+                                )
+                        );
+                    }
+                }
+            },
+            e -> getLogger().info("Can't register staff online command due to bot connection issues.")
+        );
     }
 
     @Override
